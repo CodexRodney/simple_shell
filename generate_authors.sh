34e915502c9a5ea8480b7998e816b0801e142de2
CodexRodney <rodney002mwanje@gmail.com>
2021-10-29 23:49:07 +0300

    Added Authors file

--- generate_authors.sh
+++ generate_authors.sh
@@ -8,5 +8,5 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/."
 		#This file list all inviduals having contributed content to repository
 	EOH
 	echo
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF.8 sort -uf
-}>AUTHORS
+	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
+} > AUTHORS

3b300ab97b2311076ec130321f0e9188cae42d3b
Goodness <atundeg@gmail.com>
2021-10-30 18:48:25 +0100

    added header file, 'SIMPLE_SHELL_H'

---
+++ simple_shell.h
@@ -0,0 +1,18 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+#include <dirent.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+
+/* Advanced tasks */
+
+#endif /* SIMPLE_SHELL_H */

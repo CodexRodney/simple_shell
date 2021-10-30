a58ad472a270f878075ba69c05c7d743133362fb
Goodness <atundeg@gmail.com>
2021-10-30 13:04:39 +0100

    AUTHORS file generated and added

--- generate_authors.sh
+++ generate_authors.sh
@@ -8,9 +8,5 @@ cd "$(dirname "$(readlink -f "$BASH_SOURCE")")/."
 		#This file list all inviduals having contributed content to repository
 	EOH
 	echo
-<<<<<<< HEAD
-	git log --format='%aN <%aE>' | LC_ALL=C.UTF.8 sort -uf
-=======
 	git log --format='%aN <%aE>' | LC_ALL=C.UTF-8 sort -uf
->>>>>>> 2c24f5015914613b5421d024cf1a25ea058b4351
 } > AUTHORS

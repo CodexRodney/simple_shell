32622ae201dc9d720e3bb37d59ebbbce40d7cbc8
Goodness <atundeg@gmail.com>
2021-11-04 21:28:03 +0100

    added last working code files

---
+++ main.c
@@ -0,0 +1,112 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * main - main program for a simple shell
+ *
+ * Return: always 0;
+ */
+
+int main(void)
+{
+
+	loop();
+
+	return (0);
+}
+
+/**
+ * shell_exit - function to exit
+ *
+ * Return: 0
+ */
+int shell_exit(void)
+{
+	return (0);
+}
+
+/**
+ * execute - function to execute command
+ *
+ * @args: argument to be executed
+ *
+ * Return: 1 or 0
+ */
+
+int execute(char **args)
+{
+	pid_t cpid;
+	int status;
+	char bin[1024] = "/bin/";
+	char *store = args[0], *pathname;
+	char *env_list[] = {"HOME=/home/goody", "PATH=/bin/", "USER=goody",
+	"LANGUAGE=en_NG:en", "SESSION=UBUNTU", "COMPIZ_CONFIG_PROFILE=ubuntu", NULL};
+
+
+	if (_strcmp(args[0], "exit") == 0)
+		exit(EXIT_SUCCESS);
+
+	if (strstr(args[0], bin) == NULL)
+	{
+		strcat(bin, store);
+		pathname = bin;
+	}
+	else
+		pathname = args[0];
+
+	cpid = fork();
+	if (cpid == 0)
+	{
+		if (execve(pathname, args, env_list) < 0)
+		{
+			perror("./shell");
+			free(*args);
+			free(args);
+		}
+		exit(EXIT_FAILURE);
+	}
+	else if (cpid < 0)
+		printf(RED "Error forking\n");
+	else
+		waitpid(cpid, &status, WUNTRACED);
+
+	return (1);
+}
+
+/**
+ * loop - looping the prompt
+ *
+ * Return: void
+ */
+
+void loop(void)
+{
+	size_t len = 0;
+	ssize_t nread;
+	char *line = NULL;
+	char **args;
+	int status = 1;
+
+	while (status)
+	{
+		print_prompt();
+		nread = getline(&line, &len, stdin);
+
+		if (nread == -1)
+		{
+			_putchar('\n');
+			exit(EXIT_SUCCESS);
+		}
+
+		/* start again is nothing is entered */
+		if ((nread == 1 && line[0] == '\n') || line[0] == '\0')
+		{
+			continue;
+		}
+
+		args = split_line(line);
+		status = execute(args);
+		free(args);
+	};
+	free(line);
+}
